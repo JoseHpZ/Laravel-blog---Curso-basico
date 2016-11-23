f1198ca2dd8ffd9b0824820da5e3c6355754303d
Marc Garcia <garciatorrent96@gmail.com>
2016-11-23 22:49:11 +0100

    Final, programando el Blog

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,13 +7,34 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Input;
+use App\Models\Comment;
+
 class CommentController extends Controller
 {
-    public function postCreatecomment() {
-        return "Crearmos el comentario";
-    }
+	public function postCreatecomment() {
+	    
+	    $input = Input::all();
+
+	    $comment = new Comment();
+
+	    $comment->username = $input['user'];
+	    $comment->text = $input['comment'];
+	    $comment->post_id = $input['post_id'];
+
+	    $comment->save();
+
+	    return 'Comentario creado!';
+	}
  
-    public function getDeletecomment($id) {
-        return "Borramos el comentario";
-    }
+	public function getDeletecomment($id) {
+	    $comment = Comment::find($id);
+
+	    if($comment == null)
+	    	return 'El comentario no existe';
+	    else
+	    	$comment->delete();
+
+	    return 'Comentario eliminado';
+	}
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,28 +7,76 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Models\Post;
+use App\Models\Comment;
+use Input;
+
 class PostController extends Controller
 {
  
     public function getList() {
-        return view('index');
-        return "Lista de todos los post por GET";
+
+        $data['posts'] = Post::all();
+
+        return view('index', $data);
     }
  
     public function getPost($id) {
-        return "Ver post, se pasa como parámetro la ID para buscarlo";
+        
+        $post = Post::find($id);
+
+        if($post == null)
+            return 'No existe el post';
+        else {
+            $data['post'] = $post;
+            $data['comments'] = Comment::where('post_id', $id)->get();
+            return view('post', $data);
+        }
+
     }
  
     public function postSavepost() {
-        return "Guardar post por POST";
+
+        $input = Input::all();
+
+        if(isset($input['post_id'])) {
+            $post = Post::find($input['post_id']);
+        } else {
+            $post = new Post();
+        }
+
+        $post->title = $input['title'];
+        $post->resume = $input['resume'];
+        $post->description = $input['description'];
+        $post->publish_date = $input['publish_date'];
+        $post->status = $input['status'];
+
+        $post->save(); // Guarda el objeto en la BD
+
+        return "Post guardado";
     }
  
-    public function getEditpost($id) {
-        return "Editar Post, ID para saber cual es.";
+    public function getEditpost($id = null) {
+        
+        if ($id == null)
+            return view('edit-post');
+        else {
+            $data['post'] = Post::find($id);
+            if($data['post'] == null)
+                return 'El post no existe';
+
+            return view('edit-post', $data);
+        }
+
     }
  
     public function getDeletepost($id) {
-        return "Borrar Post, ID para saber cual es.";
+        $post = Post::find($id);
+
+        if($post == null)
+            return "No existe este post";
+        else
+            $post->delete();
     }
  
 }
